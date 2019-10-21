80d2b1a024318104197b0dbe63ee30dc1391d1ce
conormcmahon <conormcmahon22@gmail.com>
2019-10-21 13:59:56 -0700

    First commit; created graph of infant mortality in several countries.

--- lab4_part3.R
+++ lab4_part3.R
@@ -17,3 +17,25 @@ library(here)
 db <- read_csv(here::here("disease_burden.csv")) %>% 
   clean_names() %>% 
   rename(deaths_per_100k = death_rate_per_100_000)
+
+
+# Comparing disease mortality for infants from the two countries with the lowest death rates, 
+#   and the two with the highest death rates
+
+target_countries <- c("Afghanistan", "United States", "Japan", "Somalia")
+
+db_sub <- db %>% 
+  filter(country_name %in% target_countries) %>% 
+  filter(age_group == "0-6 days", sex == "Both") %>% 
+  group_by(country_name)
+
+# Create a basic line graph showing the death trends over time for these countries
+
+ggplot(data = db_sub, aes(x = year, y = deaths_per_100k)) + 
+  geom_line(aes(color = country_name)) + 
+  scale_y_continuous(expand = c(0,0)) + 
+  scale_x_continuous(expand = c(0,0)) + 
+  xlab("Year") + 
+  ylab("Deaths per 100,000")
+
+ggsave(here("final_graphs", "disease_graph.png"))
